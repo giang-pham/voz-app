44b3682eab2c6b53be251c3ef4157dfb772eac25
Giang <hoang_pham@aviva-asia.com>
2016-12-01 10:46:31 +0800

    replace About with Bookmark tab

---
+++ src/pages/bookmark/bookmark.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-bookmark',
+  templateUrl: 'bookmark.html'
+})
+export class BookmarkPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
